0f02d097f9e98831b74e8f13a5829f6d27a3a66d
Skidrowskie <skidrowskie@gmail.com>
2025-05-15 19:23:43 +0800

    initial commit

--- main.py
+++ main.py
@@ -348,10 +348,12 @@ time = 0
 
 
 #Default Arguments
-def net_price(list_price, discount = 0, tax = 0.05):
-    return list_price * (1 -discount) * (1 + tax)
+# def net_price(list_price, discount = 0, tax = 0.05):
+#     return list_price * (1 -discount) * (1 + tax)
 
-print(net_price(500, 0.1, 0))
+# print(net_price(500, 0.1, 0))
 
+#Keyword Arguments
+keyword = "p"
           
     
